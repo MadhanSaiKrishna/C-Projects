dbb704b80e5e9778bb16ed3f38f606a878b4344f
Madhan <madhansaikrishna@outlook.com>
2024-01-08 00:53:29 +0530

    Added main.c

---
+++ Number System Conversion/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    int ch;
+    printf("\t\tMenu\n");
+    printf("1. Decimal to binary\n");
+    printf("2. Binary to Decimal\n");
+    printf("Enter Your Choice : ");
+    scanf("%d", &ch);
+    switch (ch)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
